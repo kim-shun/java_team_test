ac6192e8fe751f824d28f2d51d636c5bce918a36
kim-shun <kimura1417080@gmail.com>
2021-08-06 16:50:04 +0900

    forで繰り返し処理

--- src/example/JavaGouse.java
+++ src/example/JavaGouse.java
@@ -3,17 +3,20 @@ package example;
 public class JavaGouse {
 
 	public static void main(String[] args) {
-		int count = 1;
-		int sum = 1;
+//		int count = 1;
+//		int sum = 0;
+//		
+//		while (count <= 40) {
+//			sum += Math.pow(count, 4);
+//			count += 1;
+//		}
+//		System.out.println(sum);
 		
-		while (count <= 40) {
+		int sum = 0;
+		for (int count = 0; count <= 40; ++count) {
 			sum += Math.pow(count, 4);
-			count += 1;
 		}
 		System.out.println(sum);
-		
-		
-
 	}
 
 }
