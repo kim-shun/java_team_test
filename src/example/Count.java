854aed54ac12bea6ce3681f473661c67ee964462
kim-shun <kimura1417080@gmail.com>
2021-08-07 16:47:50 +0900

    一旦整理

--- src/example/Count.java
+++ src/example/Count.java
@@ -3,15 +3,17 @@ package example;
 public class Count {
 	
 	void countHi(String str) {
-		int count = 0;
+//		int count = 0;
 
-        int num = str.indexOf("hi");
+        int num = str.indexOf("hi", 1);
+        System.out.println(num);
+        //indexofの第2引数は検索開始位置の指定(ない場合は-1)
 
-        for(int i = 0; i < num+1; i++){
-            num = str.indexOf("hi", num+1);
-            count++;
-        }
-        System.out.println(count);
+//        for(int i = 0; i < num+1; i++){
+//            num = str.indexOf("hi", num+1);
+//            count++;
+//        }
+//        System.out.println(count);
 	}
 
 }
