47142b1b3688d2adafb1e469471b670c4cb6334a
kim-shun <kimura1417080@gmail.com>
2021-08-07 09:37:58 +0900

    配列の全ての値を取り出す

--- src/example/CountEvens.java
+++ src/example/CountEvens.java
@@ -3,8 +3,11 @@ package example;
 public class CountEvens {
 
 	public static void main(String[] args) {
-		int[] count_evens = {2,1,7,8,2,3,4};
-		System.out.println(count_evens[2]);
+		int nums[] = {2,1,7,8,2,3,4};
+		
+		for (int num: nums) {
+			System.out.println(num);
+		}
 	}
 
 }
