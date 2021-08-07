d16a2a6ecc5c18879cb7dddda1a227557a0157fc
kim-shun <kimura1417080@gmail.com>
2021-08-07 09:41:37 +0900

    1つの配列の偶数の数を数えることに成功

--- src/example/CountEvens.java
+++ src/example/CountEvens.java
@@ -5,9 +5,13 @@ public class CountEvens {
 	public static void main(String[] args) {
 		int nums[] = {2,1,7,8,2,3,4};
 		
+		int count = 0;
 		for (int num: nums) {
-			System.out.println(num);
+			if (num % 2 == 0) {
+				count += 1;
+			}
 		}
+		System.out.println(count);
 	}
 
 }
