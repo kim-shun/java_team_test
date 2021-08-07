8aed6d7439b823bf944e4365c52a5f81273fdc26
kim-shun <kimura1417080@gmail.com>
2021-08-07 09:33:54 +0900

    偶数を数える

--- src/example/CountEvens.java
+++ src/example/CountEvens.java
@@ -3,8 +3,8 @@ package example;
 public class CountEvens {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		int[] count_evens = {2,1,7,8,2,3,4};
+		System.out.println(count_evens[2]);
 	}
 
 }
