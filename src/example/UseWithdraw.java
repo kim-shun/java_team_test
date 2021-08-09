302f1ad084997496891b4773b7f77189b12abcd0
kim-shun <kimura1417080@gmail.com>
2021-08-09 11:07:57 +0900

    並び替え直し

--- src/example/UseWithdraw.java
+++ src/example/UseWithdraw.java
@@ -6,11 +6,10 @@ public class UseWithdraw {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
 		int balance = 100000;
-		int amount = scan.nextInt();
 		
 		System.out.println("いくら引き落としますか?（手数料110円かかります）");
+		int amount = scan.nextInt();
 		
 		Bank withdraw1 = new Bank();
 		withdraw1.Withdraw(balance, amount);
