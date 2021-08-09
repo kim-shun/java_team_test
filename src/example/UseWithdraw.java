4484a57faa4b7aa65fed9fa1e20bd63a6af1e513
kim-shun <kimura1417080@gmail.com>
2021-08-09 10:51:31 +0900

    並び替え

--- src/example/UseWithdraw.java
+++ src/example/UseWithdraw.java
@@ -6,9 +6,11 @@ public class UseWithdraw {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
 		int balance = 100000;
-		System.out.println("いくら引き落としますか?（手数料110円かかります）");
 		int amount = scan.nextInt();
+		
+		System.out.println("いくら引き落としますか?（手数料110円かかります）");
 		
 		Bank withdraw1 = new Bank();
 		withdraw1.Withdraw(balance, amount);
