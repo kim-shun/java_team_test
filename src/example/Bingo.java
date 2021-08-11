9313fb70e81f3abaac891f27728026ac93d403ca
kim-shun <kimura1417080@gmail.com>
2021-08-11 16:01:44 +0900

    ひとまず数字の出力までは完成

---
+++ src/example/Bingo.java
@@ -0,0 +1,54 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Bingo {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> list15 = new ArrayList<Integer>();
+		ArrayList<Integer> list30 = new ArrayList<Integer>();
+		ArrayList<Integer> list45 = new ArrayList<Integer>();
+		ArrayList<Integer> list60 = new ArrayList<Integer>();
+		ArrayList<Integer> list75 = new ArrayList<Integer>();
+		
+		for(int count = 1 ; count <= 15 ; count++) {
+            list15.add(count);
+        }
+		
+		for(int count = 16 ; count <= 30 ; count++) {
+            list30.add(count);
+        }
+		
+		for(int count = 31 ; count <= 45 ; count++) {
+            list45.add(count);
+        }
+		
+		for(int count = 46 ; count <= 60 ; count++) {
+            list60.add(count);
+        }
+		
+		for(int count = 61 ; count <= 75 ; count++) {
+            list75.add(count);
+        }
+		
+		Collections.shuffle(list15);
+		Collections.shuffle(list30);
+		Collections.shuffle(list45);
+		Collections.shuffle(list60);
+		Collections.shuffle(list75);
+		
+		for (int count = 0; count <= 4; count++) {
+			System.out.print(list15.get(count));
+			System.out.print(list30.get(count));
+			System.out.print(list45.get(count));
+			System.out.print(list60.get(count));
+			System.out.print(list75.get(count));
+			System.out.println("\n");
+		}
+		
+		
+
+	}
+
+}
