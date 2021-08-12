aa736845539ad37f1b9d945aeffdcea68e7d06b8
kim-shun <kimura1417080@gmail.com>
2021-08-12 09:05:12 +0900

    policeTrouble

---
+++ src/example/PoliceTrouble.java
@@ -0,0 +1,14 @@
+package example;
+
+public class PoliceTrouble {
+
+	void policeTrouble(boolean a, boolean b) {
+		if (a && b || !a && !b) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+	}
+
+
+}
---
+++ src/example/UsePoliceTrouble.java
@@ -0,0 +1,18 @@
+package example;
+
+public class UsePoliceTrouble {
+
+	public static void main(String[] args) {
+		PoliceTrouble trouble1 = new PoliceTrouble();
+		trouble1.policeTrouble(true, false);
+		
+		PoliceTrouble trouble2 = new PoliceTrouble();
+		trouble2.policeTrouble(false, false);
+		
+		PoliceTrouble trouble3 = new PoliceTrouble();
+		trouble3.policeTrouble(true, true);
+		
+
+	}
+
+}
