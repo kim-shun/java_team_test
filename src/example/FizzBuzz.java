ea63b53fb18409c43011efa8209595c2c868f6c4
kim-shun <kimura1417080@gmail.com>
2021-08-12 09:43:07 +0900

    FizzBuzz問題

---
+++ src/example/FizzBuzz.java
@@ -0,0 +1,17 @@
+package example;
+
+public class FizzBuzz {
+	void fizzBuzz() {
+		for (int count = 1; count <= 100; count++) {
+			if (count % 3 == 0 && count % 5 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (count % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (count % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(count);
+			}
+		}
+	}
+}
---
+++ src/example/UseFizzBuzz.java
@@ -0,0 +1,10 @@
+package example;
+
+public class UseFizzBuzz {
+
+	public static void main(String[] args) {
+		FizzBuzz fizzBuzz1 = new FizzBuzz();
+		fizzBuzz1.fizzBuzz();
+	}
+
+}
