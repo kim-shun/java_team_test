24bcd0bf563c565367e322588dba055bd5b169a2
kim-shun <kimura1417080@gmail.com>
2021-08-16 06:12:27 +0900

    引数3つのクラス

---
+++ src/example/Num.java
@@ -0,0 +1,13 @@
+package example;
+
+public class Num {
+	void num(int a, int b, int c) {
+		int ab = a + b;
+		if (c <= 3) {
+			System.out.println(ab / c);
+		} else {
+			System.out.println(ab * c);
+		}
+	}
+
+}
---
+++ src/example/UseNum.java
@@ -0,0 +1,17 @@
+package example;
+
+public class UseNum {
+
+	public static void main(String[] args) {
+		Num num1 = new Num();
+		num1.num(1, 5, 3);
+		
+		Num num2 = new Num();
+		num2.num(1, 5, 5);
+		
+		Num num3 = new Num();
+		num3.num(1, 9, -2);
+
+	}
+
+}
