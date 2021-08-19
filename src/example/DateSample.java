47230269495211d15f943c1e8dbd16297393fc2d
kim-shun <kimura1417080@gmail.com>
2021-08-19 09:49:30 +0900

    今日の日付を表示

---
+++ src/example/DateSample.java
@@ -0,0 +1,14 @@
+package example;
+
+import java.util.Date;
+
+public class DateSample {
+
+	public static void main(String[] args) {
+		Date date = new Date();
+		
+		System.out.println(date.toString());
+
+	}
+
+}
