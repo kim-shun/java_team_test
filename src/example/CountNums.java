2bdc4217042af4fe76c2c74c75b8924f54dcf30b
kim-shun <kimura1417080@gmail.com>
2021-08-20 21:54:43 +0900

    配列内の偶数を数える

---
+++ src/example/CountNums.java
@@ -0,0 +1,14 @@
+package example;
+
+public class CountNums {
+	void countEvens(int[] nums) {
+		int count = 0;
+		for(int num : nums) {
+			if (num % 2 == 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
