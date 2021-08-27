0093f2f5d7a922f091e5152de1bf3b428b726033
kim-shun <kimura1417080@gmail.com>
2021-08-27 17:56:32 +0900

    アウトサイドモード

---
+++ src/example/OutsideMode.java
@@ -0,0 +1,19 @@
+package example;
+
+public class OutsideMode {
+
+	public static void main(String[] args) {
+		in1to10(5, false);
+		in1to10(11,false);
+		in1to10(11,true);
+	}
+	
+	public static void in1to10(int num, boolean outsideMode) {
+		if ((num >= 1 && num <= 10) || outsideMode ) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+	}
+
+}
