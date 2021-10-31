9ba89393b3aeb08602ba66934d640838f088c28c
kim-shun <kimura1417080@gmail.com>
2021-10-31 19:37:41 +0900

    ArrayListの一例

---
+++ src/example/ArrayListSample.java
@@ -0,0 +1,15 @@
+package example;
+
+import java.util.Arrays;
+
+public class ArrayListSample {
+
+	public static void main(String[] args) {
+		char[][] arr = {{'a', 'b', 'c'}, 
+                {'a', 'b', 'c', 'd'}, 
+                {'a', 'b', 'c', 'd', 'e'}};
+		
+		System.out.println(Arrays.deepToString(arr));
+	}
+
+}
