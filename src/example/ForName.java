e878bb420c6f0fb9f47c94742cb51796f44f1369
kim-shun <kimura1417080@gmail.com>
2021-12-04 13:42:24 +0900

    forの復習

---
+++ src/example/ForName.java
@@ -0,0 +1,14 @@
+package example;
+
+public class ForName {
+
+	public static void main(String[] args) {
+		String name[] = {"Suzuki", "Katou", "Yamada"};
+		for (String str: name) {
+			System.out.println(str);
+			}
+		
+
+	}
+
+}
