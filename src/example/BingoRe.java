37915f1989398a2af85be1a4a0803a6dfb784fbb
kim-shun <kimura1417080@gmail.com>
2022-01-29 06:41:17 +0900

    1~15をリストに格納

---
+++ src/example/BingoRe.java
@@ -0,0 +1,15 @@
+package example;
+
+public class BingoRe {
+
+	public static void main(String[] args) {
+		int[] B = new int[15];
+		for (int i =0; i < 15; i++) {
+			B[i] = i + 1;
+			System.out.println(B[i]);
+		}
+		
+
+	}
+
+}
