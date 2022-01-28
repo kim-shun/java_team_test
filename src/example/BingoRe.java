fc0202bccd70f337185a4f1fa0a75fc84ffdc95f
kim-shun <kimura1417080@gmail.com>
2022-01-29 06:45:06 +0900

    75までの数字をリストに格納

--- src/example/BingoRe.java
+++ src/example/BingoRe.java
@@ -4,9 +4,19 @@ public class BingoRe {
 
 	public static void main(String[] args) {
 		int[] B = new int[15];
+		int[] I = new int[15];
+		int[] N = new int[15];
+		int[] G = new int[15];
+		int[] O = new int[15];
+		
 		for (int i =0; i < 15; i++) {
 			B[i] = i + 1;
-			System.out.println(B[i]);
+			I[i] = i + 16;
+			N[i] = i + 31;
+			G[i] = i + 46;
+			O[i] = i + 61;
+			
+			System.out.println(O[i]);
 		}
 		
 
