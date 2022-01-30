94050a713c2c235dfce0028ea1c61e13e80efca3
kim-shun <kimura1417080@gmail.com>
2022-01-30 09:10:40 +0900

    配列をリストに変更してみる

--- src/example/BingoRe.java
+++ src/example/BingoRe.java
@@ -1,25 +1,27 @@
 package example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BingoRe {
 
 	public static void main(String[] args) {
-		int[] B = new int[15];
+		List<Integer> B = new ArrayList<Integer>();
+		
 		int[] I = new int[15];
 		int[] N = new int[15];
 		int[] G = new int[15];
 		int[] O = new int[15];
 		
 		for (int i =0; i < 15; i++) {
-			B[i] = i + 1;
+			B.add(i + 1);
 			I[i] = i + 16;
 			N[i] = i + 31;
 			G[i] = i + 46;
 			O[i] = i + 61;
-			
-			System.out.println(O[i]);
+			System.out.println(B.get(i));
 		}
 		
-
 	}
 
 }
