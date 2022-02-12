0f5db73d9f147188bf0575274aaeb61d6e87fd2a
kim-shun <kimura1417080@gmail.com>
2022-02-13 08:58:55 +0900

    電話帳作成問題の準備

---
+++ src/example/CreatePhonebook.java
@@ -0,0 +1,21 @@
+package example;
+
+public class CreatePhonebook {
+
+	public static void main(String[] args) {
+		String[] names1 = {"キシモト", "イトウ", "ババ", "カネダ", "ワダ", "ハマダ"};
+		String[] names2 = {"フクダ", "キムラ", "タナカ", "ハシモト", "オカモト", "ゴトウ"};
+		String[] names3 = {"ワタナベ", "ヨシダ", "マツモト", "ナガタ", "ドウモト", "タチバナ","チバ"};
+		PhoneBook(names1);
+		PhoneBook(names2);
+		PhoneBook(names3);
+
+	}
+	
+	static void PhoneBook(String[] names) {
+		
+	}
+	
+	
+
+}
