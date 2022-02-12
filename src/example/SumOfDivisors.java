ff01a00a3c6011a21140fd952d9f094b99697858
kim-shun <kimura1417080@gmail.com>
2022-02-12 22:09:48 +0900

    制限付きの約数の和

--- src/example/SumOfDivisors.java
+++ src/example/SumOfDivisors.java
@@ -11,6 +11,9 @@ public class SumOfDivisors {
 		scan = new Scanner(System.in);
 		System.out.println("数字を入力してください");
 		int num = scan.nextInt();
+		System.out.println("約数の和を出す範囲を指定してください");
+		int range = scan.nextInt();
+		
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		int sum = 0;
 		for (int i = 1; i <= num; i++) {
@@ -19,9 +22,17 @@ public class SumOfDivisors {
 				sum += i;
 			}
 		}
+		
+		int sum2 = 0;
+		for (int j = 1; j <= range; j++) {
+			if (num % j == 0) {
+				sum2 += j;
+			}
+		}
 		System.out.println("約数の数は" + list.size() + "です");
 		System.out.println("約数の総和は" + sum + "です");
-		System.out.println(list);
+		System.out.println(range +"以下の約数の和は" + sum2 + "です");
+		
 		
 
 	}
