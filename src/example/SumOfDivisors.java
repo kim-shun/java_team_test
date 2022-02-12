e1b174a50a1bf8f031f96368377d879615e1b502
kim-shun <kimura1417080@gmail.com>
2022-02-12 21:52:45 +0900

    約数の総和を算出するプログラム

--- src/example/SumOfDivisors.java
+++ src/example/SumOfDivisors.java
@@ -12,11 +12,15 @@ public class SumOfDivisors {
 		System.out.println("数字を入力してください");
 		int num = scan.nextInt();
 		ArrayList<Integer> list = new ArrayList<Integer>();
+		int sum = 0;
 		for (int i = 1; i <= num; i++) {
 			if (num % i == 0) {
 				list.add(i);
+				sum += i;
 			}
 		}
+		System.out.println("約数の数は" + list.size() + "です");
+		System.out.println("約数の総和は" + sum + "です");
 		System.out.println(list);
 		
 
