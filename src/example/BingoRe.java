ad7c4ea75a4605a40cfc84ac13d86ba02529c075
kim-shun <kimura1417080@gmail.com>
2022-02-12 09:20:24 +0900

    強引な中央揃え

--- src/example/BingoRe.java
+++ src/example/BingoRe.java
@@ -32,11 +32,11 @@ public class BingoRe {
 		System.out.println("  B |  I |  N |  G |  O");
 		
 		for (int j= 0; j < 5; j++) {
-			System.out.printf("%4d%s",B.get(j),"|");
-			System.out.printf("%4d%s",I.get(j),"|");
-			System.out.printf("%4d%s",N.get(j),"|");
-			System.out.printf("%4d%s",G.get(j),"|");
-			System.out.printf("%4d",O.get(j));
+			System.out.printf("%3d%s",B.get(j)," |");
+			System.out.printf("%3d%s",I.get(j)," |");
+			System.out.printf("%3d%s",N.get(j)," |");
+			System.out.printf("%3d%s",G.get(j)," |");
+			System.out.printf("%3d",O.get(j));
 			System.out.println();
 		}
 		
