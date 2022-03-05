ee5bc1d44585d15038448c2bd39e59c02191109f
kim-shun <kimura1417080@gmail.com>
2022-03-05 21:13:57 +0900

    カレンダーの微修正

--- src/example/CreateCalendar3.java
+++ src/example/CreateCalendar3.java
@@ -12,7 +12,7 @@ public class CreateCalendar3 {
 		int thisMonth = cl.get(Calendar.MONTH); //今月(1月=0、2月=1であるため配列monthの添字に使用)
 		outputCalendar(thisYear,thisMonth);
 		
-		//確認用
+		//他の年、他の月の場合の確認用
 		System.out.println("\n");
 		int otherYear = 2013; //西暦を入力
 		int otherMonth = 4; //月を入力
@@ -23,19 +23,17 @@ public class CreateCalendar3 {
 	public static void outputCalendar(int inputYear, int inputMonth) {
 		Calendar cl = Calendar.getInstance();
 		
-		String[] monthName = {"January","February","March","April","May","June",
+		String[] month = {"January","February","March","April","May","June",
 				"July","August","September","October","November","December"};
 		String[] week = {"Su","Mo","Tu","We","Th","Fr","Sa"};
 		
-		int year = inputYear;
-		int month = inputMonth; 
 		int firstDay = 1; //月の初日
-		//今月の初日の曜日(日曜日=1、月曜日=2であるため-1をして配列weekの添字に使用)
-		cl.set(year,month,firstDay);
+		//月の初日の曜日(日曜日=1、月曜日=2であるため-1をして配列weekの添字に使用)
+		cl.set(inputYear,inputMonth,firstDay);
 		int firstDayWeek = cl.get(Calendar.DAY_OF_WEEK) - 1; 
 		int lastDay = cl.getActualMaximum(Calendar.DAY_OF_MONTH); //月の最終日
 
-		System.out.print("     " + monthName[month] + " " +  year);
+		System.out.print("     " + month[inputMonth] + " " +  inputYear); //年と月を出力
 		System.out.println(); 
 
 		//カレンダーの曜日を出力
@@ -43,7 +41,7 @@ public class CreateCalendar3 {
 			System.out.printf("%2s ",week[i]);
 		}
 		System.out.println();
-		//初日の位置を曜日に合わせて空白を出力
+		//初日が何曜日かに合わせて空白を出力
 		System.out.print(String.join("", Collections.nCopies(firstDayWeek, "   ")));
 		//日付を出力
 		for (int i = firstDay; i <= lastDay; i++) {
