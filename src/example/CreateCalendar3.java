51cc20bf1b55f01ed889aab735ec7031a729c138
kim-shun <kimura1417080@gmail.com>
2022-03-05 09:16:24 +0900

    強引に完成させたカレンダー

--- src/example/CreateCalendar3.java
+++ src/example/CreateCalendar3.java
@@ -8,33 +8,36 @@ public class CreateCalendar3 {
 
 	public static void main(String[] args) {
 		Calendar cl = Calendar.getInstance();
+	
 		String[] month = {"January","February","March","April","May","June",
 				"July","August","September","October","November","December"};
 		String[] week = {"","Su","Mo","Tu","We","Th","Fr","Sa"};
 		
-		int lastDay = cl.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int thisYear = cl.get(Calendar.YEAR); //今年
+		int thisMonth = cl.get(Calendar.MONTH); //今月(1月=0、2月=1であるため配列monthの添字に使用)
+		cl.set(thisYear,thisMonth,1); //今月の初日
+		int firstDayWeek = cl.get(Calendar.DAY_OF_WEEK); //今月の初日の曜日(日曜日=1、月曜日=2であるため配列weekの添字に使用)	
+		int lastDay = cl.getActualMaximum(Calendar.DAY_OF_MONTH); //今月の最終日
+		
+		//初日から最終日までの日付をlistに格納
 		ArrayList<Integer> dates = new ArrayList<Integer>();
 		for (int i = 1; i <= lastDay; i++) {
 			dates.add(i);
 		}
-		int thisYear = cl.get(Calendar.YEAR);
-		int thisMonth = cl.get(Calendar.MONTH);
-		cl.set(thisYear,thisMonth,1);
-		int firstDayWeek = cl.get(Calendar.DAY_OF_WEEK);
 		
 		System.out.print(month[thisMonth]);
 		System.out.println(thisYear);   
 
+		//カレンダーの曜日を表示
 		for (int i = 1; i < week.length; i++) {
 			System.out.printf("%2s ",week[i]);
 		}
 		System.out.println();
 		
-		System.out.print(String.join("", Collections.nCopies(firstDayWeek, " ")));
+		System.out.print(String.join("", Collections.nCopies(firstDayWeek-1, "   ")));
 		for (int i = 0; i < lastDay; i++) {
 			System.out.printf("%2d ",dates.get(i));
-
-			if ((firstDayWeek + i - 1) % 7 == 0) {
+			if ((firstDayWeek + 1 - i) % 7 == 0) {
 				System.out.println();
 			}
 		}
